3f50a3c6f49b04b7b8499eb0d446ef427e86ebba
Dana F <danatania@gmail.com>
2018-05-28 21:50:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  console.log('hello')
-  return string.toLowerCase();
-}  
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+  } else if (string.toUpperCase() === string) {
+   return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+};
